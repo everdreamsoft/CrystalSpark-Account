1dedfa1d94d7b47bb763b0c53027fae3779df27d
shaban shaame <shaban@everdreamsoft.com>
2020-11-29 10:56:51 +0100

    Account sign and users

--- examples/roles.php
+++ examples/roles.php
@@ -6,11 +6,44 @@
  * Time: 12:50
  */
 
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
+use CsAccount\CSAccountManager;
 use CsAccount\CsAccountRole;
+use CsAccount\CSAccountUser;
 
-$accountManager = \CsAccount\CSAccountManager::initWithDebugServerKey();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$adminRole = new CsAccountRole('admin');
-$memberRole = new CsAccountRole('member');
+
+
+$adminAddress = '0xB5529f45D86b0B7ce0bd1679a86E3d13f936573C';
+$userAddress = '0x1E6638CA282D643e5c9a1651BC894A8e527D78b2';
+
+$anAdmin = new CsAccountRole('admin');
+$aUser = new CsAccountRole('user');
+$aGuest = new CsAccountRole('guest');
+
+$csAccountManager = CSAccountManager::initWithDebugServerKey()->withCookies();
+CSAccountUser::initWithName("belovedAdmin")
+    ->setRoles([$anAdmin])
+    ->setAuthEthSignAddresses([$adminAddress])
+    ->addToManager($csAccountManager);
+
+CSAccountUser::initWithName("respectedMember")
+    ->setRoles([$aUser])
+    ->setAuthEthSignAddresses([$userAddress])
+    ->addToManager($csAccountManager);
+
+if ($csAccountManager->hasRole(new CsAccountRole('admin'))){
+    echo "admin";
+} else echo "not admin";
+
+echo PHP_EOL ;
+
+if ($csAccountManager->hasRole(new CsAccountRole('user'))){
+    echo "user";
+} else echo "not user";
+
 
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -22,8 +22,39 @@ use CsAccount\CsAccountECDSA ;
 final class UserTest extends TestCase
 {
 
+    const PASSPHRASE_TEST = 'decline surprise mother rally fall gauge robust dentist sight goose peanut interest';
 
-    public function testUser(){
+    const ADMIN_JWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2NhbGhvc3Q6ODg4OCIsImF1ZCI6ImxvY2FsaG9zdDo4ODg4IiwiaWF0IjoxNjA2NTk4ODY3LCJpcCI6Ijo6MSIsInZlcmlmaWVkIjp'
+    .'0cnVlLCJhZGRyZXNzIjoiMHhiNTUyOWY0NWQ4NmIwYjdjZTBiZDE2NzlhODZlM2QxM2Y5MzY1NzNjIn0.y_tE1pHNRRwnbS-Guo0nUBFU28JllO0sLfBgOFtp3z0';
+
+    private function buildManager(){
+
+        $adminAddress = '0xB5529f45D86b0B7ce0bd1679a86E3d13f936573C';
+        $userAddress = '0x1E6638CA282D643e5c9a1651BC894A8e527D78b2';
+
+        $anAdmin = new CsAccountRole('admin');
+        $aUser = new CsAccountRole('user');
+        $aGuest = new CsAccountRole('guest');
+
+        $csAccountManager = CSAccountManager::initWithDebugServerKey();
+        CSAccountUser::initWithName("belovedAdmin")
+            ->setRoles([$anAdmin])
+            ->setAuthEthSignAddresses([$adminAddress])
+            ->addToManager($csAccountManager);
+
+        CSAccountUser::initWithName("respectedMember")
+            ->setRoles([$aUser])
+            ->setAuthEthSignAddresses([$userAddress])
+            ->addToManager($csAccountManager);
+
+        return $csAccountManager ;
+
+    }
+
+
+    public function testUserManager(){
+
+        $adminAddress = '0xB5529f45D86b0B7ce0bd1679a86E3d13f936573C';
 
         $anAdmin = new CsAccountRole('admin');
         $aUser = new CsAccountRole('user');
@@ -37,9 +68,59 @@ final class UserTest extends TestCase
 
 
         $this->assertCount(1,$csAccountManager->userManager->getAddressMap());
+        $this->assertEquals($csAccountManager->userManager->getAddressMap()[strtolower($adminAddress)]->getName(),'belovedAdmin');
 
     }
 
+    public function testGetUserFromJWT(){
+
+        $jwt = self::ADMIN_JWT ;
+
+        $csAccountManager = $this->buildManager();
+        $this->assertTrue($csAccountManager->isRegisteredUser($jwt));
+
+
+    }
+
+    public function testTwoUserSameAddress(){
+
+        $jwt = self::ADMIN_JWT ;
+
+        $csAccountManager = $this->buildManager();
+        $this->assertTrue($csAccountManager->isRegisteredUser($jwt));
+
+        $userDuplicateUser = new CsAccountRole('userDuplicateUser');
+
+        $userAccepted = true ;
+        try {
+
+            CSAccountUser::initWithName("belovedAdmin")
+                ->setRoles([$userDuplicateUser])
+                ->setAuthEthSignAddresses(['0xB5529f45D86b0B7ce0bd1679a86E3d13f936573C'])
+                ->addToManager($csAccountManager);
+
+        }catch (Exception $e){
+
+            $userAccepted = false ;
+        }
+
+        $this->assertFalse($userAccepted);
+
+    }
+
+    public function testHasRole(){
+
+        $jwt = self::ADMIN_JWT ;
+
+        $csAccountManager = $this->buildManager();
+        $this->assertTrue($csAccountManager->hasRole(new CsAccountRole('admin'),$jwt));
+        $this->assertFalse($csAccountManager->hasRole(new CsAccountRole('user'),$jwt));
+
+
+
+
+
+    }
 
 
 
